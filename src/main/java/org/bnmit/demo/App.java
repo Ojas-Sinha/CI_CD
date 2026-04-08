80b7b264490bb2312ace4c4bfeacd22ae422d735
Ojas Sinha <ojassinha14@gmail.com>
2026-04-09 00:27:59 +0530

    Update App.java

--- src/main/java/org/bnmit/demo/App.java
+++ src/main/java/org/bnmit/demo/App.java
@@ -15,10 +15,7 @@ public class App {
 
         // ✅ Frontend (HTML page)
         server.createContext("/", exchange -> {
-            String html = "<html><body>"
-                    + "<h1>CI/CD Pipeline Demo</h1>"
-                    + "<a href='/greet'>Click to Call API</a>"
-                    + "</body></html>";
+            String html = "<html><body><h1>CI/CD Pipeline Demo</h1></body></html>";
 
             exchange.getResponseHeaders().add("Content-Type", "text/html");
             exchange.sendResponseHeaders(200, html.length());
@@ -28,17 +25,6 @@ public class App {
             os.close();
         });
 
-        // ✅ API Endpoint
-        server.createContext("/greet", exchange -> {
-            String response = greet();
-
-            exchange.sendResponseHeaders(200, response.length());
-            OutputStream os = exchange.getResponseBody();
-            os.write(response.getBytes());
-            os.close();
-        });
-
         server.start();
-        System.out.println("Server running at http://localhost:8080/");
     }
 }
